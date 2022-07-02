b8073a61d98223a0e52e3ac16e9cbd33bccadd19
Serena71 <serena991031zs@gmail.com>
2022-07-02 17:11:33 +1000

    build api function

---
+++ frontend/src/components/API.jsx
@@ -0,0 +1,56 @@
+export const apiPost = async (path, content) => {
+  // return new Promise((resolve, reject) => {
+  const init = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+    body: JSON.stringify(content),
+  };
+  const res = await fetch(`http://127.0.0.1:5500/${path}`, init);
+  // await console.log(res);
+  const body = await res.json();
+  return body;
+};
+
+function objToQueryString(obj) {
+  const keyValuePairs = [];
+  for (const key in obj) {
+    keyValuePairs.push(encodeURIComponent(key) + '=' + encodeURIComponent(obj[key]));
+  }
+  return keyValuePairs.join('&');
+}
+
+export const apiGet = async (path, paramDic) => {
+  // return new Promise((resolve, reject) => {
+  const init = {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+  };
+  const params = objToQueryString(paramDic);
+  const res = await fetch(`http://127.0.0.1:5500/${path}?${params}`, init);
+  // await console.log(res);
+  const body = await res.json();
+  if (body.errortype == 1) {
+    alert(body.errorMessage);
+  } else {
+    return body.errorMessage;
+  }
+};
+
+export const apiCall = async (path, paramDic) => {
+  const params = objToQueryString(paramDic);
+  const res = await fetch(`http://127.0.0.1:5500/${path}?${params}`);
+  // await console.log(res);
+  const body = await res.json();
+  console.log(body);
+  if (body.errortype == 1) {
+    alert(body.errorMessage);
+  } else {
+    return body.errorMessage;
+  }
+};
