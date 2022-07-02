b239690f17870a64fb58e57a4671e1701bd30dc2
Serena71 <serena991031zs@gmail.com>
2022-07-02 18:01:18 +1000

    jump from home page to login page if not logged in

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import NavBar from '../components/NavBar';
-
+import { useNavigate } from 'react-router-dom';
 
 export default function HomePage () {
+    const navigate = useNavigate()
+
+    React.useEffect(() => {
+        if (!localStorage.getItem('name')) {
+            navigate('/login')
+            alert('Your are not logged in')
+        }
+    }, [])
     return <>
         <NavBar />
     </>
