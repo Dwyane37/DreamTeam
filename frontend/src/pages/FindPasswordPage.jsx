05f0e5faaf97cf8486ce6eb866e737c6b825ec2d
Serena71 <serena991031zs@gmail.com>
2022-07-02 22:33:11 +1000

    fix component name type

--- frontend/src/pages/FindPasswordPage.jsx
+++ frontend/src/pages/FindPasswordPage.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import CodeVerify from '../components/CodeVerify';
-import ChangePassword from '../components/FindPassword';
+import FindPassword from '../components/FindPassword';
+
 export default function FindPasswordPage() {
   const [codeCorrect, setCodeCorrect] = React.useState(null);
   return (
     <div className="find_password_page">
       <h1>Find Your Password</h1>
       {!codeCorrect && <CodeVerify verifyResult={[codeCorrect, setCodeCorrect]} />}
-      {codeCorrect && <ChangePassword />}
+      {codeCorrect && <FindPassword />}
     </div>
   );
 }
