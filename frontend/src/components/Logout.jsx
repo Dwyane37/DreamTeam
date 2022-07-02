c84b98e37fcfe038d90b5514ebdf730b9e09b904
Serena71 <serena991031zs@gmail.com>
2022-07-02 17:14:24 +1000

    build logout component

---
+++ frontend/src/components/Logout.jsx
@@ -0,0 +1,61 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogTitle from '@mui/material/DialogTitle';
+import { apiCall } from '../components/API';
+import MenuItem from '@mui/material/MenuItem';
+import { useNavigate } from 'react-router-dom';
+
+
+export default function Logout () {
+  const [open, setOpen] = React.useState(false);
+  const navigate = useNavigate();
+
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleLogOut = () => {
+    apiCall('user/logout', { id: 1 }).then((body) => {
+      console.log(body);
+      setOpen(false);
+      navigate('/')
+    });
+
+  };
+
+  return (
+    <div>
+      {/* <Button variant="outlined" onClick={handleClickOpen}>
+        Logout
+      </Button> */}
+      <MenuItem onClick={handleClickOpen}>Logout</MenuItem>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{'Are you sure to log out?'}</DialogTitle>
+        {/* <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Let Google help apps determine location. This means sending anonymous location data to Google, even when no
+            apps are running.
+          </DialogContentText>
+        </DialogContent> */}
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={handleLogOut} autoFocus>
+            Confirm
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
