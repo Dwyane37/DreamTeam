c1f1df08714e500b7473bec57aefe6a84fe46e2b
Serena71 <serena991031zs@gmail.com>
2022-07-04 22:47:04 +1000

    remove headings in find password page

--- frontend/src/pages/FindPasswordPage.jsx
+++ frontend/src/pages/FindPasswordPage.jsx
@@ -6,7 +6,6 @@ export default function FindPasswordPage() {
   const [codeCorrect, setCodeCorrect] = React.useState(null);
   return (
     <div className="find_password_page">
-      <h1>Find Your Password</h1>
       {!codeCorrect && <CodeVerify verifyResult={[codeCorrect, setCodeCorrect]} />}
       {codeCorrect && <FindPassword />}
     </div>
