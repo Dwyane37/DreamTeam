59fdc8448d7f3e66078efebef2a244a36781d03f
climberzbm <baominzhang0616@gmail.com>
2022-07-15 17:37:07 +0800

    resume api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,23 +8,25 @@ import FindPasswordPage from './pages/FindPasswordPage';
 import HomePage from './pages/HomePage';
 import SettingsPage from './pages/SettingsPage';
 import ResumePage from './pages/ResumePage';
+import FollowPage from './pages/FollowPage'
 
 function App() {
   return (
     <div>
       <Router>
         <Routes>
-          <Route path="/" element={<WelcomePage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/find_password" element={<FindPasswordPage />} />
-          <Route path="/home" element={<HomePage />} />
-          <Route path="/settings" element={<SettingsPage />} />
-          <Route path="/profile" element={<ResumePage />} />
+          <Route path='/' element={<WelcomePage />} />
+          <Route path='/login' element={<LoginPage />} />
+          <Route path='/register' element={<RegisterPage />} />
+          <Route path='/find_password' element={<FindPasswordPage />} />
+          <Route path='/home' element={<HomePage />} />
+          <Route path='/settings' element={<SettingsPage />} />
+          <Route path='/profile' element={<ResumePage />} />
+          <Route path='/follow' element={<FollowPage />} />
         </Routes>
       </Router>
     </div>
-  );
+  )
 }
 
 export default App;
