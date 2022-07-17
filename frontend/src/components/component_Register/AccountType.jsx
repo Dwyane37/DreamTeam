a6b02bf1a2aeac2e685bfee2c7f5c6838e2d3f6b
Serena71 <serena991031zs@gmail.com>
2022-07-17 18:52:29 +1000

    add some basic css on register and login page

--- frontend/src/components/component_Register/AccountType.jsx
+++ frontend/src/components/component_Register/AccountType.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Button from '@mui/material/Button';
-
+import './AccountType.css';
 function AccountType(props) {
   const typename = props.typename;
   let accHandler = props.handler;
   return (
     <Button
       style={{ backgroundColor: props.colour }}
+      className="account-type-button"
       variant="contained"
       onClick={() => {
         accHandler(typename);
--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -23,9 +23,9 @@ function RegisterPage() {
       ) : (
         <>
           <h1>Select your account type</h1>
-          <div>
-            <AccountType typename="student" colour="orange" handler={updateState} />
-            <AccountType typename="company" handler={updateState} />
+          <div className="selectAccountType">
+            <AccountType typename="student" colour="#5B9279" handler={updateState} />
+            <AccountType typename="company" colour="#91785D" handler={updateState} />
           </div>
         </>
       )}
