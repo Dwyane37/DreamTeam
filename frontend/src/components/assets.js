c03cb5ca7f197cb5ceabe66d1ba810d26d69be17
Serena71 <serena991031zs@gmail.com>
2022-07-17 16:49:38 +1000

    include assets file

---
+++ frontend/src/components/assets.js
@@ -0,0 +1,17 @@
+export const citizenship = [
+  { id: 1, label: 'Australian Citizen' },
+  { id: 2, label: 'Australian PR' },
+  { id: 3, label: 'New Zealand Citizen' },
+  { id: 4, label: ' New Zealand PR' },
+  { id: 5, label: ' Working Visa Holder' },
+  { id: 6, label: ' International Students' },
+];
+
+export const fields = [
+  { id: 1, label: 'Information Technology' },
+  { id: 2, label: 'Law' },
+  { id: 3, label: 'Science' },
+  { id: 4, label: ' Statistics' },
+  { id: 5, label: ' Medicine' },
+  { id: 6, label: ' Engineering' },
+];
