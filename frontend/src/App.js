ab0517f68700050812a2fb778b87f54db4882d1e
Serena71 <serena991031zs@gmail.com>
2022-08-07 14:01:23 +1000

    add socket for notification catch and display

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useNavigate, Link } from 'react-router-dom';
+
 import WelcomePage from './pages/WelcomePage';
 import SignInSide from './pages/SignIn';
 import SignUp from './pages/SignUpPage';
@@ -15,18 +16,48 @@ import AppliedJobs from './pages/AppliedJobs';
 import ResumePage from './pages/ResumePage';
 import FollowPage from './pages/FollowPage';
 import { io } from 'socket.io-client';
+import Snackbar from '@mui/material/Snackbar';
+import Button from '@mui/material/Button';
+import Alert from '@mui/material/Alert';
+import SnackbarContent from '@mui/material/SnackbarContent';
+// import Link from '@mui/material/Link';
 
 function App() {
+  // const navigate = useNavigate();
   const [socket, setSocket] = React.useState(null);
+  const [state, setState] = React.useState({ open: false, msg: '', hr: '' });
+  const handleClose = () => {
+    setState({ open: false, msg: '', hr: '' });
+  };
 
   React.useEffect(() => {
     console.log('connect');
     setSocket(io('http://localhost:5005'));
   }, []);
+  React.useEffect(() => {
+    sessionStorage.getItem('id') ? socket?.emit('newUser', sessionStorage.getItem('id')) : null;
+    socket?.on('getNotification', (res) => {
+      const hr = res.sender;
+      setState({ open: true, msg: 'Your followed employer just posted a new job', hr: hr });
+    });
+  }, [socket]);
+
+  const action = (
+    <Link style={{ color: 'white' }} onClick={handleClose} to={`/profile/${state.hr}`}>
+      Click to view
+    </Link>
+  );
 
   return (
     <div>
       <Router>
+        <Snackbar
+          anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+          open={state.open}
+          onClose={handleClose}
+          message={state.msg}
+          action={action}
+        />
         <Routes>
           <Route path="/" element={<SignInSide />} />
           <Route path="/login" element={<SignInSide />} />
